56cd0c0e304ce572ff956f34e858d21b121752d7
John Edcel Recio <jedcelfrecio@gmail.com>
2024-02-09 11:24:36 +0800

    feat: Change Customer class to Person

---
+++ ReceptionAide/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceptionAide
+{
+    public class Person
+    {
+
+        private string name;
+        private Sex sex;
+        private DateTime birthDate;
+        private Address address;
+
+        public Person(string name, Sex sex, DateTime birthDate)
+        {
+
+            this.name = name;
+            this.sex = sex;
+            this.birthDate = birthDate;
+            this.address = new Address();
+
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public Sex GetSex()
+        {
+            return this.sex;
+        }
+
+        public void SetSex(Sex sex)
+        {
+            this.sex = sex;
+        }
+
+        public DateTime GetBirthDate()
+        {
+            return this.birthDate;
+        }
+
+        public void SetBirthDate(DateTime birthDate)
+        {
+            this.birthDate = birthDate;
+        }
+
+        public Address GetAddress()
+        {
+            return this.address;
+        }
+
+    }
+
+}
