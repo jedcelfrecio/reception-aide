5ab20a911a3d3ea56f12f5cd6673f1ec8cc12e4a
John Edcel Recio <jedcelfrecio@gmail.com>
2024-02-10 16:51:16 +0800

    feat: Create Employee class

---
+++ ReceptionAide/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceptionAide
+{
+    public class Employee : Person
+    {
+
+        private List<Service> servicesOffered;
+
+        public Employee(string name, Sex sex, DateTime birthDate) : base(name, sex, birthDate)
+        {
+            this.servicesOffered = new List<Service>();
+        }
+
+        public List<Service> GetServicesOffered()
+        {
+            return this.servicesOffered;
+        }
+
+        public void AddService(Service service)
+        {
+            servicesOffered.Add(service);
+        }
+
+        public void RemoveService(Service service)
+        {
+            servicesOffered.Remove(service);
+        }
+
+    }
+
+}
