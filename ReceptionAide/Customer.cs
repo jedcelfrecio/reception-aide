e93fa0bb97bce048a91ebfa732ecf1282a1d649b
John Edcel Recio <jedcelfrecio@gmail.com>
2024-02-10 16:53:57 +0800

    feat: Create Customer class

---
+++ ReceptionAide/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceptionAide
+{
+    public class Customer : Person
+    {
+
+        public Customer(string name, Sex sex, DateTime birthDate) : base(name, sex, birthDate) { }
+
+    }
+
+}
