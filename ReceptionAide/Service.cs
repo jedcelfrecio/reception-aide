7169003b8a6b4a9b3be7cffc4dc04b37cbd04174
John Edcel Recio <jedcelfrecio@gmail.com>
2024-02-10 16:51:00 +0800

    feat: Override Equals for Service class

--- ReceptionAide/Service.cs
+++ ReceptionAide/Service.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +15,27 @@ namespace ReceptionAide
         public Service(string name)
         {
             this.name = name;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public override bool Equals(Object? service)
+        {
+
+            Service serviceToCompare = (Service)service;
+
+            bool servicesHaveTheSameName = (this.name.Equals(serviceToCompare.GetName()));
+
+            return servicesHaveTheSameName;
+
         }
 
     }
